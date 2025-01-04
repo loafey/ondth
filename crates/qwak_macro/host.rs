5ba71bd0b60d8ede475f0a0a4a0334decdd7b93c
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-04 16:25:33 +0100

    Host function macros are now working nicely

--- crates/qwak_macro/host.rs
+++ crates/qwak_macro/host.rs
@@ -1,13 +1,105 @@
 use proc_macro::TokenStream as TS;
-use proc_macro2::Span;
 use quote::quote;
-use syn::{
-    FnArg, Ident, ItemTrait, Pat, PatIdent, ReturnType, TraitItem, parse_macro_input,
-    punctuated::Punctuated, token::Comma,
-};
+use syn::{FnArg, ItemTrait, Pat, TraitItem, Type, punctuated::Punctuated, token::Comma};
+
+fn stringify(ty: &Type) -> String {
+    match ty {
+        Type::Path(type_path) => type_path
+            .path
+            .segments
+            .iter()
+            .map(|ps| ps.ident.to_string())
+            .collect::<Vec<_>>()
+            .join("::"),
+        Type::Array(_) => todo!("type_array"),
+        Type::BareFn(_) => todo!("type_bare_fn"),
+        Type::Group(_) => todo!("type_group"),
+        Type::ImplTrait(_) => todo!("type_impl_trait"),
+        Type::Infer(_) => todo!("type_infer"),
+        Type::Macro(_) => todo!("type_macro"),
+        Type::Never(_) => todo!("type_never"),
+        Type::Paren(_) => todo!("type_paren"),
+        Type::Ptr(_) => todo!(")"),
+        Type::Reference(_) => todo!("type_reference"),
+        Type::Slice(_) => todo!("type_slice"),
+        Type::TraitObject(_) => todo!("type_trait_object"),
+        Type::Tuple(_) => todo!("type_tuple"),
+        Type::Verbatim(_) => todo!("token_stream"),
+        _ => panic!(),
+    }
+}
+fn get_type(ty: &Type) -> Type {
+    let ty = stringify(ty);
+    let id = match &*ty {
+        "String" => quote!(qwak::PTR),
+        "u64" => quote!(qwak::ValType::I64),
+        _ => panic!("undefined type conversion for \"{ty}\""),
+    };
+    syn::parse(id.into()).unwrap()
+}
 
 pub fn get_export_functions(item: TS) -> TS {
-    quote! {}.into()
+    let tree = syn::parse::<ItemTrait>(item).unwrap();
+
+    let mut funcs = quote! {};
+    let mut defs = quote! {};
+    for func in tree.items {
+        let TraitItem::Fn(func) = func else {
+            panic!("only functions are supported")
+        };
+        let sig = func.sig;
+        let func_name = sig.ident.clone();
+        let string_name = format!("{func_name}");
+        let inputs = &sig.inputs;
+        let mut args: Punctuated<_, Comma> = Punctuated::new();
+        let mut ins: Punctuated<_, Comma> = Punctuated::new();
+        let mut out: Punctuated<_, Comma> = Punctuated::new();
+        sig.inputs.iter().for_each(|f| {
+            let FnArg::Typed(pt) = f else {
+                panic!("self arguments are not allowed");
+            };
+            let Pat::Ident(id) = &*pt.pat else {
+                panic!("only idents are allowed")
+            };
+            ins.push(get_type(&pt.ty));
+            args.push(id.ident.clone());
+        });
+        match sig.output {
+            syn::ReturnType::Default => {}
+            syn::ReturnType::Type(_, ty) => out.push(get_type(&ty)),
+        }
+        funcs = quote! {
+            #funcs
+            qwak::Function::new(
+                #string_name,
+                [#ins],
+                [#out],
+                qwak::UserData::Rust(std::sync::Mutex::new(()).into()),
+                #func_name
+            ),
+        };
+        defs = quote! {
+            #defs
+            qwak::host_fn!(pub #func_name(#inputs) -> () {
+                use qwak_shared::QwakHostFunctions;
+                Ok(super::$name:: #func_name (#args))
+            });
+        };
+    }
+    quote! {
+        #[macro_export]
+        macro_rules! host_gen {
+            ($name:ident) => {
+                pub mod inner {
+                    pub fn functions() -> impl IntoIterator<Item = qwak::Function> {
+                        [#funcs]
+                    }
+                    #defs
+                }
+            }
+        }
+    }
+    .into()
 }
 
 pub fn get_host_calls(item: TS) -> TS {
--- src/qwak_host_functions.rs
+++ src/qwak_host_functions.rs
@@ -1,64 +1,30 @@
 #![allow(static_mut_refs)]
-use faststr::FastStr;
-use qwak::{Function, PTR, UserData, ValType};
-
 use crate::{
     get_nw,
     net::server::{NW_PTR, transmit_message},
 };
+pub use inner::functions as qwak_functions;
+use qwak_shared::QwakHostFunctions;
 
-pub fn qwak_functions() -> impl IntoIterator<Item = Function> {
-    [
-        Function::new(
-            "debug_log",
-            [PTR],
-            [],
-            UserData::Rust(std::sync::Mutex::new(()).into()),
-            debug_log,
-        ),
-        Function::new(
-            "broadcast_message",
-            [PTR],
-            [],
-            UserData::Rust(std::sync::Mutex::new(()).into()),
-            broadcast_message,
-        ),
-        Function::new(
-            "get_player_name",
-            [ValType::I64],
-            [PTR],
-            UserData::Rust(std::sync::Mutex::new(()).into()),
-            get_player_name,
-        ),
-    ]
-}
+qwak_shared::host_gen!(Host);
+struct Host;
+impl QwakHostFunctions for Host {
+    fn get_player_name(id: u64) -> String {
+        let (nw, _) = get_nw!();
+        let name = nw
+            .lobby
+            .get(&id)
+            .map(|pi| pi.name.to_string())
+            .unwrap_or_else(|| "unknown player".to_string());
+        name
+    }
 
-fn inner_get_player_name(id: u64) -> String {
-    let (nw, _) = get_nw!();
-    let name = nw
-        .lobby
-        .get(&id)
-        .map(|pi| pi.name.clone())
-        .unwrap_or_else(|| FastStr::from("unknown player"));
-    name.to_string()
-}
-qwak::host_fn!(get_player_name(id: u64) -> String {
-    Ok(inner_get_player_name(id))
-});
+    fn debug_log(value: String) {
+        println!("{value}");
+    }
 
-fn inner_debug_log(value: String) {
-    println!("{value}");
+    fn broadcast_message(value: String) {
+        let (nw, server) = get_nw!();
+        transmit_message(server, nw, value);
+    }
 }
-qwak::host_fn!(debug_log(value: String) {
-    inner_debug_log(value);
-    Ok(())
-});
-
-fn inner_broadcast_message(value: String) {
-    let (nw, server) = get_nw!();
-    transmit_message(server, nw, value);
-}
-qwak::host_fn!(broadcast_message(value: String) {
-    inner_broadcast_message(value);
-    Ok(())
-});
