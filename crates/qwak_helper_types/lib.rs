23e0eb2670b1f277f3b00b7a77eb035e0725e487
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-05 22:02:29 +0100

    Document `qwak_helper_types`

--- crates/qwak_helper_types/lib.rs
+++ crates/qwak_helper_types/lib.rs
@@ -1,10 +1,16 @@
+//! Contains multiple types which help are used when calling `qwak` plugin
+//! methods.
 use extism_pdk::{FromBytes, Msgpack, ToBytes};
 use serde::{Deserialize, Serialize};
 
+/// The argument to [`map_interact`](../qwak_shared/trait.QwakPlugin.html#tymethod.map_interact).
 #[derive(Debug, Clone, FromBytes, ToBytes, Deserialize, Serialize)]
 #[encoding(Msgpack)]
 pub struct MapInteraction {
+    /// The script to run
     pub script: String,
+    /// The optional target entity
     pub target: Option<String>,
+    /// The id of the player activating the interaction
     pub player_id: u64,
 }
