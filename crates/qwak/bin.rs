a57a88ee0ed3f166102d376bb40d249c259a4824
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-05 22:45:06 +0100

    Remove qwak bin

--- crates/qwak/bin.rs
+++
@@ -1,7 +0,0 @@
-#![allow(clippy::print_stdout)]
-fn main() {
-    let plugin =
-        qwak::QwakPlugin::new("target/wasm32-unknown-unknown/debug/default.wasm", vec![]).unwrap();
-    println!("{:?}", plugin.plugin_name());
-    println!("Running version: {:?}", plugin.plugin_version());
-}
