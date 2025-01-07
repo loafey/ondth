845a2ca74ca16deec93003d57a1c8b4c7d8cc378
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-07 20:00:22 +0100

    Disable one more log

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ fn main() {
                 plug
             })
             .set(LogPlugin {
-                filter: "bevy_ecs=error,wgpu=error,naga=warn,present_frames=warn,cosmic_text=warn,bevy_render=warn,offset_allocator=warn,extism_plugin=warn,extism=warn,winit=warn,bevy_winit=warn,bevy_hanabi=warn,bevy_app=warn,wasmtime=warn,bevy_asset=warn,gilrs=warn"
+                filter: "bevy_ecs=error,wgpu=error,naga=warn,present_frames=warn,cosmic_text=warn,bevy_render=warn,offset_allocator=warn,extism_plugin=warn,extism=warn,winit=warn,bevy_winit=warn,bevy_hanabi=warn,bevy_app=warn,wasmtime=warn,bevy_asset=warn,gilrs=warn,bevy_hierarchy=warn"
                     .into(),
                 level: bevy::log::Level::DEBUG,
                 ..default()
