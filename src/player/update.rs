8dd74a5b00066073335f91e0552b4a7152097dde
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-01-15 16:19:26 +0100

    Maybe?

--- src/player/update.rs
+++ src/player/update.rs
@@ -332,25 +332,27 @@ impl Player {
 
             let hort_speed = player.hort_speed;
             let y_before = player.velocity.y;
+            let mut temp = Vec3::ZERO;
             if keys.walk_forward_pressed && !paused.0 {
-                player.velocity = forward * hort_speed * time.delta_secs();
+                temp += forward * hort_speed;
                 player.camera_movement.backdrift_goal = player.camera_movement.backdrift_max;
             } else if keys.walk_backward_pressed && !paused.0 {
-                player.velocity = -forward * hort_speed * time.delta_secs();
+                temp += -forward * hort_speed;
                 player.camera_movement.backdrift_goal = -player.camera_movement.backdrift_max;
             } else {
                 player.camera_movement.backdrift_goal = 0.0;
             }
 
             if keys.walk_left_pressed && !paused.0 {
-                player.velocity = -right * hort_speed * time.delta_secs();
+                temp += -right * hort_speed;
                 player.camera_movement.cam_rot_goal = player.camera_movement.cam_rot_max_goal;
             } else if keys.walk_right_pressed && !paused.0 {
-                player.velocity = right * hort_speed * time.delta_secs();
+                temp += right * hort_speed;
                 player.camera_movement.cam_rot_goal = -player.camera_movement.cam_rot_max_goal;
             } else {
                 player.camera_movement.cam_rot_goal = 0.0;
             }
+            player.velocity = temp * time.delta_secs();
             player.velocity.y = y_before;
 
             if !keys.walk_left_pressed
